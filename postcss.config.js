a2977a8eadd35a2e65ee2377091f10f79e587fee
Sir-takbiri <sir.takbiri.github@gmail.com>
2024-12-18 10:07:23 +0330

    aaaaa

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+const autoprefixer = require("autoprefixer");
+const { plugin } = require("postcss");
+
+MediaSourceHandle.exports ={
+    plugin:{
+        tailwindcss: {},
+        autoprefixer: {},
+    }
+}
